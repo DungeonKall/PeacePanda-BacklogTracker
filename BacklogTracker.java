b84a229d54a594e3ee3b4fe413f0d80dba822dde
DungeonKall <kpnolan2001+github@gmail.com>
2023-10-25 13:40:58 -0700

    Basic head file, to be added to further or used as base branch to make branches and changes from.

---
+++ BacklogTracker.java
@@ -0,0 +1,14 @@
+/*
+    Here's a great place to put our names and info about the code!
+ */
+
+
+public class BacklogTracker {
+
+    // What is yall's comment preference? Above or on side?
+    // Main method
+    public static void main(String[] args) {        // Main method
+        System.out.println("Welcome to your personalized game backlog tracker!" +
+                "\nIt is not fully implemented yet!");
+    }
+}
